ec50e86a84a8e7a0118de16d0cba96413f8d9e55
Miles Bardon <milesbardon@gmail.com>
2024-12-02 09:40:17 +0000

    Refactoring to OOP

--- src/main/java/org/aoc/Day2.java
+++ src/main/java/org/aoc/Day2.java
@@ -9,6 +9,51 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
+class Report {
+    List<Integer> levels;
+
+    public Report(List<Integer> levels) {
+        this.levels = levels;
+    }
+
+    public Report(String input) {
+        this(Arrays.stream(input.split(" ")).map(Integer::valueOf).toList());
+    }
+
+    public boolean isSafe() {
+        List<Integer> clonedLevels = new ArrayList<>(this.levels);
+        Collections.sort(clonedLevels);
+
+        if (!this.levels.equals(clonedLevels) && !this.levels.equals(clonedLevels.reversed())) {
+            return false;
+        }
+
+        for (int i = 0; i < this.levels.size() - 1; i++) {
+            int currLevel = this.levels.get(i);
+            int nextLevel = this.levels.get(i + 1);
+            int diff = Math.abs(currLevel - nextLevel);
+
+            if (diff < 1 || diff > 3) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public List<Report> createPermutations() {
+        List<Report> permutations = new ArrayList<>();
+
+        for (int i = 0; i < this.levels.size(); i++) {
+            List<Integer> clonedLevels = new ArrayList<>(this.levels);
+            clonedLevels.remove(i);
+            permutations.add(new Report(clonedLevels));
+        }
+
+        return permutations;
+    }
+}
+
 public class Day2 {
     public static void main(String[] args) throws IOException {
         String input = FileUtils.openInput("Day2.txt");
@@ -17,76 +62,33 @@ public class Day2 {
         System.out.println("Day 2 - Part 2: " + part2(input));
     }
 
-   static List<List<Integer>> parseInput(String input) {
-       List<List<Integer>> reports = new ArrayList<>();
+    static List<Report> parseInput(String input) {
+        List<Report> reports = new ArrayList<>();
 
-       input.lines().forEach(report -> {
-          reports.add(Arrays.stream(report.split(" ")).map(Integer::valueOf).toList());
-       });
+        input.lines().forEach(line -> {
+            reports.add(new Report(line));
+        });
 
-       return reports;
-   }
+        return reports;
+    }
 
-   static boolean isSafe(List<Integer> report) {
-       // Check if report is identical when sorted
+    public static long part1(String input) {
+        return parseInput(input).stream().filter(Report::isSafe).count();
+    }
 
-       List<Integer> cloneReport = new ArrayList<>(report);
-       Collections.sort(cloneReport);
+    public static int part2(String input) {
+        List<Report> reports = parseInput(input);
 
-       if (!report.equals(cloneReport) && !report.equals(cloneReport.reversed())) {
-           return false;
-       }
+        AtomicInteger safeReports = new AtomicInteger();
 
-       for (int i = 0; i < report.size() - 1; i++) {
-           int currLevel = report.get(i);
-           int nextLevel = report.get(i + 1);
-           int diff = Math.abs(currLevel - nextLevel);
+        reports.forEach(report -> {
+            if (report.isSafe()) {
+                safeReports.getAndIncrement();
+            } else if (report.createPermutations().stream().anyMatch(Report::isSafe)) {
+                safeReports.getAndIncrement();
+            }
+        });
 
-           if (diff < 1 || diff > 3 ) {
-               return false;
-           }
-       }
-
-       return true;
-   }
-
-   public static int part1(String input) {
-       List<List<Integer>> reports = parseInput(input);
-
-       AtomicInteger safeReports = new AtomicInteger();
-
-       reports.forEach(report -> {
-           if (isSafe(report)) safeReports.getAndIncrement();
-       });
-
-       return safeReports.get();
-   }
-
-   public static int part2(String input) {
-       List<List<Integer>> reports = parseInput(input);
-
-       AtomicInteger safeReports = new AtomicInteger();
-
-       reports.forEach(report -> {
-           if (isSafe(report)) {
-               safeReports.getAndIncrement();
-           } else {
-               // Create permutations of reports with 1 level missing
-
-               List<List<Integer>> permutations = new ArrayList<>();
-
-               for (int i = 0; i < report.size(); i++) {
-                   List<Integer> clonedReport = new ArrayList<>(report);
-                   clonedReport.remove(i);
-                   permutations.add(clonedReport);
-               }
-
-               if (permutations.stream().anyMatch(Day2::isSafe)) {
-                   safeReports.getAndIncrement();
-               }
-           }
-       });
-
-       return safeReports.get();
-   }
+        return safeReports.get();
+    }
 }
