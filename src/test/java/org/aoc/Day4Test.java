af417223dde522101e57ed87ea8f812ed9026ed2
Miles Bardon <milesbardon@gmail.com>
2024-12-04 10:02:30 +0000

    Adding test for part 1

--- src/test/java/org/aoc/Day4Test.java
+++ src/test/java/org/aoc/Day4Test.java
@@ -1,7 +1,11 @@
 package org.aoc;
 
+import org.aoc.utils.FileUtils;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,10 +22,22 @@ public class Day4Test {
             MAMMMXMMMM
             MXMXAXMASX
             """;
+    static String realInput;
+
+    @BeforeAll
+    static void readInput() throws IOException {
+        realInput = FileUtils.openInput("Day4.txt");
+    }
 
     @Test
     @DisplayName("Should find all the XMAS words in the grid")
     void testPart1Example() {
         assertEquals(18, Day4.part1(exampleInput));
+    }
+
+    @Test
+    @DisplayName("Part 1 should return 2390")
+    void testPart1() {
+        assertEquals(2390, Day4.part1(realInput));
     }
 }
