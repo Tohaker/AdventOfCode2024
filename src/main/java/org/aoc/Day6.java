39032314d93a888ac029e9a7dcae667ea5e137bc
Miles Bardon <milesbardon@gmail.com>
2024-12-06 16:27:55 +0000

    Starting Part 2

--- src/main/java/org/aoc/Day6.java
+++ src/main/java/org/aoc/Day6.java
@@ -95,6 +95,112 @@ public class Day6 {
     }
 
     public static int part2(String input) {
-        return 0;
+        // Parse input
+
+        List<Point> obstacles = new ArrayList<>();
+        Set<Point> positions = new HashSet<>();
+        Point start = null;
+        Point currentPosition = null;
+        Point nextPosition = null;
+        String direction = "N";
+
+        int height = 0;
+        int width = 0;
+
+        for (String line : input.lines().toList()) {
+            width = 0;
+
+            for (String cell : Arrays.stream(line.split("")).toList()) {
+                if (cell.equals("#")) {
+                    obstacles.add(new Point(width ,height));
+                } else if (cell.equals("^")) {
+                    start = new Point(width, height);
+                    nextPosition = new Point(start);
+                }
+                width++;
+            }
+            height++;
+        }
+
+        int loopsFound = 0;
+
+        // Loop through permutations of obstacles
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                Point newObstacle = new Point(x, y);
+
+                if (obstacles.contains(newObstacle)) {
+                    continue;
+                }
+
+                // Start walking
+
+                int stepCount = 0;
+
+                while (nextPosition.getY() < height && nextPosition.getX() < width) {
+                    if (obstacles.contains(nextPosition) || nextPosition.equals(newObstacle)) {
+                        switch (direction) {
+                            case "N": {
+                                direction = "E";
+                                break;
+                            }
+                            case "E": {
+                                direction = "S";
+                                break;
+                            }
+                            case "S": {
+                                direction = "W";
+                                break;
+                            }
+                            case "W": {
+                                direction = "N";
+                                break;
+                            }
+                        }
+
+                        // Reset nextPosition to currentPosition, as we can't go past an obstacle.
+                        nextPosition = currentPosition;
+                    } else {
+                        currentPosition = nextPosition;
+                        positions.add(currentPosition);
+                    }
+
+                    switch (direction) {
+                        case "N": {
+                            nextPosition = new Point(nextPosition.x, nextPosition.y - 1);
+                            break;
+                        }
+                        case "E": {
+                            nextPosition = new Point(nextPosition.x + 1, nextPosition.y);
+                            break;
+                        }
+                        case "S": {
+                            nextPosition = new Point(nextPosition.x, nextPosition.y + 1);
+                            break;
+                        }
+                        case "W": {
+                            nextPosition = new Point(nextPosition.x - 1, nextPosition.y);
+                            break;
+                        }
+                    }
+
+                    if (stepCount == positions.size() * 2) {
+                        loopsFound++;
+                        break;
+                    }
+
+                    stepCount++;
+                }
+
+                // Reset for next permutation
+
+                positions = new HashSet<>();
+                nextPosition = new Point(start);
+                direction = "N";
+            }
+        }
+
+        return loopsFound;
     }
 }
