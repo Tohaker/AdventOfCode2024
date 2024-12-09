326c49abb26f974942082d7f4394c167091a1962
Miles Bardon <milesbardon@gmail.com>
2024-12-09 09:37:43 +0000

    Completing Part 1

---
+++ src/main/java/org/aoc/Day9.java
@@ -0,0 +1,58 @@
+package org.aoc;
+
+import org.aoc.utils.FileUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Day9 {
+    public static void main(String[] args) throws IOException {
+        String input = FileUtils.openInput("Day9.txt");
+
+        System.out.println("Day 9 - Part 1: " + part1(input));
+//        System.out.println("Day 9 - Part 2: " + part2(input));
+    }
+
+    public static long part1(String input) {
+        List<Integer> blocks = new ArrayList<>();
+
+        for (int i = 0; i < input.length(); i += 2) {
+            int index = i / 2;
+            int files = input.charAt(i) - '0'; // Effective conversion of char to int for ASCII characters
+            int freeSpace = 0;
+
+            if (i + 1 < input.length()) {
+                freeSpace = input.charAt(i + 1) - '0';
+            }
+
+            blocks.addAll(Collections.nCopies(files, index));
+            blocks.addAll(Collections.nCopies(freeSpace, null));
+        }
+
+        for (int i = 0; i < blocks.size(); i++) {
+            if (blocks.get(i) == null) {
+              blocks.set(i, blocks.getLast());
+              blocks.removeLast();
+              while (blocks.getLast() == null) {
+                  blocks.removeLast();
+              }
+            }
+        }
+
+        blocks.removeAll(Collections.singleton(null));
+
+        long checksum = 0;
+
+        for (int i = 0; i < blocks.size(); i++) {
+            checksum += blocks.get(i) * i;
+        }
+
+        return checksum;
+    }
+
+    public static long part2(String input) {
+        return 0;
+    }
+}
---
+++ src/test/java/org/aoc/Day9Test.java
@@ -0,0 +1,38 @@
+package org.aoc;
+
+import org.aoc.utils.FileUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day9Test {
+    static String exampleInput = "2333133121414131402";
+    static String realInput;
+
+    @BeforeAll
+    static void readInput() throws IOException {
+        realInput = FileUtils.openInput("Day9.txt");
+    }
+
+    @Test
+    @DisplayName("Should defragment and checksum the filesystem")
+    void testPart1Example() {
+        assertEquals(1928, Day9.part1(exampleInput));
+    }
+
+    @Test
+    @DisplayName("Part 1 should return 6356833654075")
+    void testPart1() {
+        assertEquals(Long.parseLong("6356833654075"), Day9.part1(realInput));
+    }
+
+    @Test
+    @DisplayName("Should defragment whole blocks and checksum the filesystem")
+    void testPart2Example() {
+        assertEquals(2858, Day9.part1(exampleInput));
+    }
+}
