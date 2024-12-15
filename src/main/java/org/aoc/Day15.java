ccae99dddf1e85524c4ea594d508b13b7f64ee77
Miles Bardon <milesbardon@gmail.com>
2024-12-15 15:54:11 +0000

    Starting Part 2

--- src/main/java/org/aoc/Day15.java
+++ src/main/java/org/aoc/Day15.java
@@ -13,18 +13,22 @@ class WarehouseItem {
     Point position;
     List<Point> walls;
     List<WarehouseItem> boxes;
+    String representation;
 
-    WarehouseItem(int x, int y, List<Point> walls, List<WarehouseItem> boxes) {
+    WarehouseItem(int x, int y, String representation, List<Point> walls, List<WarehouseItem> boxes) {
         this.position = new Point(x, y);
         this.walls = walls;
         this.boxes = boxes;
+        this.representation = representation;
     }
 
     public int getGPSCoordinate() {
+        if (representation.equals("]")) return 0;
+
         return 100 * position.y + position.x;
     }
 
-    public boolean move(String direction) {
+    public boolean move(String direction, boolean checkOther) {
         Point nextPosition = new Point(position);
 
         switch (direction) {
@@ -46,11 +50,52 @@ class WarehouseItem {
             }
         }
 
-        var boxesInNextPosition = boxes.stream().filter(b -> b.position.equals(nextPosition)).toList();
+        var boxesInNextPosition = boxes
+                .stream()
+                .filter(b -> b.position.equals(nextPosition))
+                .toList();
+
+        var verticalMovement = direction.equals("^") || direction.equals("v");
 
         if (!boxesInNextPosition.isEmpty()) {
-           var moved = boxesInNextPosition.getFirst().move(direction);
-           if (!moved) return false;
+            var nextBox = boxesInNextPosition.getFirst();
+            if (checkOther && verticalMovement) {
+                int newX = nextPosition.x;
+
+                if (nextBox.representation.equals("[")) {
+                    newX++;
+                } else if (nextBox.representation.equals("]")) {
+                    newX--;
+                }
+
+                int finalNewX = newX;
+                var boxesInOtherPosition = boxes
+                        .stream()
+                        .filter(b -> b.position.equals(new Point(finalNewX, nextPosition.y)))
+                        .toList();
+
+                if (!boxesInOtherPosition.isEmpty()) {
+                    var moved = nextBox.move(direction, true) && boxesInOtherPosition.getFirst().move(direction, true);
+                    if (!moved) return false;
+                }
+            } else {
+                var moved = boxesInNextPosition.getFirst().move(direction, false);
+                if (!moved) return false;
+            }
+        }
+
+        if (checkOther && verticalMovement && !representation.equals("O") && !representation.equals("@")) {
+            int newX = nextPosition.x;
+
+            if (representation.equals("[")) {
+                newX++;
+            } else if (representation.equals("]")) {
+                newX--;
+            }
+
+            if (walls.contains(new Point(newX, nextPosition.y)) || walls.contains(nextPosition)) {
+                return false;
+            }
         }
 
         if (walls.contains(nextPosition)) {
@@ -67,11 +112,11 @@ public class Day15 {
         String input = FileUtils.openInput("Day15.txt");
 
         System.out.println("Day 15 - Part 1: " + part1(input));
-//        System.out.println("Day 15 - Part 2: " + part2(input));
+        System.out.println("Day 15 - Part 2: " + part2(input));
     }
 
     public static int part1(String input) {
-        var splitted = input.split(System.lineSeparator() + System.lineSeparator());
+        var splitted = input.split("\\n\\n");
         var grid = splitted[0];
         var instructions = Arrays.stream(splitted[1].lines().collect(Collectors.joining()).split("")).toList();
 
@@ -87,9 +132,9 @@ public class Day15 {
                 if (line.get(x).equals("#")) {
                     walls.add(new Point(x, y));
                 } else if (line.get(x).equals("O")) {
-                    boxes.add(new WarehouseItem(x, y, walls, boxes));
+                    boxes.add(new WarehouseItem(x, y, line.get(x), walls, boxes));
                 } else if (line.get(x).equals("@")) {
-                    robot = new WarehouseItem(x, y, walls, boxes);
+                    robot = new WarehouseItem(x, y, line.get(x), walls, boxes);
                 }
             }
         }
@@ -101,11 +146,91 @@ public class Day15 {
         }
 
         for (String instruction : instructions) {
-            robot.move(instruction);
+            robot.move(instruction, false);
         }
 
         for (WarehouseItem box : boxes) {
             total += box.getGPSCoordinate();
+        }
+
+        return total;
+    }
+
+    public static int part2(String input) {
+        var splitted = input.split("\\n\\n");
+        var grid = splitted[0];
+        var instructions = Arrays.stream(splitted[1].lines().collect(Collectors.joining()).split("")).toList();
+
+        List<Point> walls = new ArrayList<>();
+        List<WarehouseItem> boxes = new ArrayList<>();
+        WarehouseItem robot = null;
+
+        grid = grid
+                .replaceAll("#", "##")
+                .replaceAll("O", "[]")
+                .replaceAll("\\.", "..")
+                .replace("@", "@.");
+
+        var lines = grid.lines().toList();
+
+        for (int y = 0; y < lines.size(); y++) {
+            var line = Arrays.stream(lines.get(y).split("")).toList();
+            for (int x = 0; x < line.size(); x++) {
+                if (line.get(x).equals("#")) {
+                    walls.add(new Point(x, y));
+                } else if (line.get(x).equals("[") || line.get(x).equals("]")) {
+                    boxes.add(new WarehouseItem(x, y, line.get(x), walls, boxes));
+                } else if (line.get(x).equals("@")) {
+                    robot = new WarehouseItem(x, y, line.get(x), walls, boxes);
+                }
+            }
+        }
+
+        int total = 0;
+
+        if (robot == null) {
+            return total;
+        }
+
+        for (String instruction : instructions) {
+            robot.move(instruction, true);
+        }
+
+        for (WarehouseItem box : boxes) {
+            total += box.getGPSCoordinate();
+        }
+
+        StringBuilder newGrid = new StringBuilder();
+        for (int y = 0; y < lines.size(); y++) {
+            var line = Arrays.stream(lines.get(y).split("")).toList();
+            StringBuilder newLine = new StringBuilder();
+            for (int x = 0; x < line.size(); x++) {
+                Point p = new Point(x, y);
+
+                if (walls.contains(p)) {
+                    newLine.append("#");
+                    continue;
+                }
+
+                var boxList = boxes
+                        .stream()
+                        .filter(b -> b.position.equals(p))
+                        .toList();
+
+                if (!boxList.isEmpty()) {
+                    newLine.append(boxList.getFirst().representation);
+                    continue;
+                }
+
+                if (p.equals(robot.position)) {
+                    newLine.append("@");
+                    continue;
+                }
+
+                newLine.append(".");
+            }
+
+            newGrid.append(newLine).append("\n");
         }
 
         return total;
--- src/test/java/org/aoc/Day15Test.java
+++ src/test/java/org/aoc/Day15Test.java
@@ -71,4 +71,52 @@ public class Day15Test {
     void testPart1() {
         assertEquals(1538871, Day15.part1(realInput));
     }
+
+    @Test
+    @DisplayName("Should calculate the sum of all GPS coords of big boxes in example 1")
+    void testPart2Example1() {
+        String exampleInput = """
+                #######
+                #...#.#
+                #.....#
+                #..OO@#
+                #..O..#
+                #.....#
+                #######
+                
+                <vv<<^^<<^^
+                """;
+
+        assertEquals(105 + 207 + 306, Day15.part2(exampleInput));
+    }
+
+    @Test
+    @DisplayName("Should calculate the sum of all GPS coords of big boxes in example 2")
+    void testPart2Example2() {
+        String exampleInput = """
+                ##########
+                #..O..O.O#
+                #......O.#
+                #.OO..O.O#
+                #..O@..O.#
+                #O#..O...#
+                #O..O..O.#
+                #.OO.O.OO#
+                #....O...#
+                ##########
+                
+                <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+                vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+                ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+                <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+                ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+                ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+                >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+                <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+                ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+                v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+                """;
+
+        assertEquals(9021, Day15.part2(exampleInput));
+    }
 }
